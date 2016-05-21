e607f010a61cbc28b40bce1cb31f2b621a965a88
Zvigun Alexandr <mircamnia@ukr.net>
2016-05-21 13:44:04 +0300

    Initial Commit

---
+++ Sale of goods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sale of goods
+//
+//  Created by Mac on 21.05.16.
+//  Copyright © 2016 Tsvigun Alexandr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
